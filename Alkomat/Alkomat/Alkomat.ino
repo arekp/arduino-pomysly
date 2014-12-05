383800f88e70395c0dc5fa7ba845181b0ad3c33e
arek.ptak@gmail.com <arek.ptak@gmail.com@9fcbf3c3-d55c-9f71-c349-e2ba0eee1267>
2014-12-05 13:06:19 +0000

    Dodano wyliczanie mb/l i promili. Posprzatanie kodu.

--- Alkomat/Alkomat/Alkomat.ino
+++ Alkomat/Alkomat/Alkomat.ino
@@ -1,86 +1,89 @@
+/* Alkomat na podstawie czujnika MQ-3
+ */
 
-int sensorPin = A0;    // numer portu analogowego
+int sensorPin = A0;    // numer portu analogowego na którym czytamy wartosc
 
 //int ledPin = 13;      // dioda LED
-int inData =3;
+int inData =3; // Pin DATA z czujnika przekroczenie progu wykrywania 
 
 int test=0; //ilosc w test
-int kalib=0; // Kalibracja
-int poziomsum=0; //pomiar suma
+int kalib=0; // Kalibracja okreslenie wartosci poczatkowej
+int poziomsum=0; //pomiar suma ze wskaznika podczas pomiaru
 int wskaznik =0;
-int leds[] = {5, 6, 7, 8, 9, 10, 11, 12, 13,4};
+
+int leds[] = {
+  5, 6, 7, 8, 9, 10, 11, 12, 13,4};
 #define SIZE 10
 
 void setup() { 
- for (int x = 0; x < SIZE; x++) {
+  Serial.begin(9600);
+  for (int x = 0; x < SIZE; x++) {
     pinMode(leds[x], OUTPUT);
   }
-  
   pinMode(inData,INPUT);
-  Serial.begin(9600);
+/* Przy pierszym odpaleniu potrzeba nagrzania grzałki */
   for(int i=1; i<17;i++){
     Serial.println("grzejemy...");
     delay(200);
   }
-
   Serial.println("kalibracja ...");
   for(int i=1;i<11;i++){
-      digitalWrite(leds[i-1], HIGH);
+    digitalWrite(leds[i-1], HIGH);
     int pomiar = analogRead(sensorPin);
     test = test + pomiar;
     Serial.print("..");
     delay(500);
   }
-  kalib=test/10;
-  Serial.print("kalibracja");
-  Serial.println(kalib);
- 
+  kalib=test/10; // wyliczamy wartość poczatkową
+    Serial.print("kalibracja"); Serial.println(kalib);
 }
 
 void loop() {
- // digitalWrite(w1, HIGH);
   Serial.println("Start pomiaru");
-Serial.println("Dmuchaj !!!!!");
-//digitalWrite(w10, HIGH);
+  Serial.println("Dmuchaj !!!!!");
 
   for(int h=1;h<11;h++){
     digitalWrite(leds[h-1], HIGH);
-    int poziom = analogRead(sensorPin);
+    int poziom = analogRead(sensorPin); //Czytamy wartosc z czujnika
     poziomsum = poziomsum+poziom;
-    int inAL = digitalRead(inData);
-    Serial.print("data: - > "); Serial.println(inAL);
+    int inAL = digitalRead(inData); //Czytamy stan ustawiony na czujniku 0 / 1
+    Serial.print("data: - > "); 
+    Serial.println(inAL);
     Serial.println(poziom);
     delay(400);
   }
+ 
+  int poziomAl = poziomsum/10; // Obliczamy usredniona wartosc z pomiaru
+  Serial.print("serdni wynik"); Serial.println(poziomAl);
+
+  wskaznik = poziomAl - kalib; // wartość powyzej danych startowych
+  Serial.print("wynik powyzej otoczenia: "); Serial.println(wskaznik);
+
+long mgCalk = map(poziomAl,0,1023,0,10);// wartosc w mg/L dla całkowitego pomiaru
+  Serial.print("Wartosc całkowita mg/l: "); Serial.println(mgCalk);
+long promile =  mgCalk*2.1;
+  Serial.print("Promile: "); Serial.println(promile);
+Serial.println("-------------------------------------------------");
+long mgCzastkowe = map(poziomAl,0,1023,0,10);
+  Serial.print("Wartosc czastkowa mg/l: "); Serial.println(mgCzastkowe);
   
-//digitalWrite(w10, LOW);
-  int poziomAl = poziomsum/10;
-  Serial.print("serdni wynik");
-  Serial.println(poziomAl);
-
-  wskaznik = poziomAl - kalib;
-
-  Serial.print("wynik powyzej otoczenia: ");
-  Serial.println(wskaznik);
-
-delay(5000);
-
-
- for (int x = 0; x < SIZE; x++) {
+/*Czyszenie wskaznika */
+  for (int x = 0; x < SIZE; x++) {
     digitalWrite(leds[x], LOW);
   }
-int val = map(wskaznik, 0, 300, 0, 10);
+  int val = map(wskaznik, 0, 300, 0, 10);
   Serial.print("Wyswietlam wartos na wy swietlaczu : ");
   Serial.println(val);
-  
- for (int x = 0; x < val; x++) {
+
+  for (int x = 0; x < val; x++) {
     digitalWrite(leds[x], HIGH);
   }
   delay(5000);
-   for (int x = 0; x < SIZE; x++) {
+  for (int x = 0; x < SIZE; x++) {
     digitalWrite(leds[x], LOW);
   }
   poziomsum=0;
-wskaznik=0;
+  wskaznik=0;
 }
+
 
